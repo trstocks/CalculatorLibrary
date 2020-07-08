1487d0a7edc3a0f087d90240c738841d7f77b654
trstocks <slipstreamanthem@gmail.com>
2020-07-07 21:11:37 -0400

    corrected flake8 errors, added division function

--- calculator.py
+++ calculator.py
@@ -10,5 +10,10 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     return first_term - second_term
 
+
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    return first_term / second_term
--- test_calculator.py
+++ test_calculator.py
@@ -18,4 +18,3 @@ class TestCalculator:
 
     def test_division(self):
         assert 25 == calculator.divide(100, 4)
-
