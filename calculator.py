64815a11aae857fe8e828b198c35aebc00ec067d
trstocks <slipstreamanthem@gmail.com>
2020-07-07 20:04:53 -0400

    add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator library containing basic math operations
+"""
+
+def add(first_term,second_term):
+    return first_term + second_term
+
+def subract(first_term,second_term):
+    return first_term - second_term
