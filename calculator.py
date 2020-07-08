6c197dade2f79bdd0101483c205ea058fd9e46fd
trstocks <slipstreamanthem@gmail.com>
2020-07-07 21:07:13 -0400

    corrected flake8 errors, added division test

--- calculator.py
+++ calculator.py
@@ -10,5 +10,5 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     return first_term - second_term
 
-def multiply(first_term,second_term):
+def multiply(first_term, second_term):
     return first_term * second_term
